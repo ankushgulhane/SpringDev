ede2527d5b7c82f3de9208cbd7bdd4e3ed7ffff4
Sourabh <Sourabh@Ankush-pc>
2017-06-21 20:52:50 +0530

    test

---
+++ src/com/b1/beans/Student.java
@@ -0,0 +1,28 @@
+package com.b1.beans;
+
+public class Student {
+	
+	private String name;
+	private long sno;
+	private String collage;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getSno() {
+		return sno;
+	}
+	public void setSno(long sno) {
+		this.sno = sno;
+	}
+	public String getCollage() {
+		return collage;
+	}
+	public void setCollage(String collage) {
+		this.collage = collage;
+	}
+	
+
+}
---
+++ src/com/b1/test/Test.java
@@ -0,0 +1,8 @@
+package com.b1.test;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+	}
+}
